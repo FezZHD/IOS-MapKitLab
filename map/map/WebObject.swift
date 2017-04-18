7eff1cc68244c27f267d110bf9cf0c55f4fc3ae1
FezZHD <fezzdev@gmail.com>
2017-04-19 02:18:53 +0300

    adding classes for working with weather stats

---
+++ map/map/WebObject.swift
@@ -0,0 +1,25 @@
+//
+//  WebObject.swift
+//  map
+//
+//  Created by Evgeni' Roslik on 19/04/2017.
+//  Copyright © 2017 Evgeni' Roslik. All rights reserved.
+//
+
+import Foundation
+
+
+public class WeatherService{
+
+    init(){
+        
+    }
+    
+    
+    public func GetWeatherInfo(idArray: [Int]) -> [WeatherStats]{
+        
+        
+
+        return [WeatherStats]();
+    }
+}
